dc7df55d764186960e14ce2fa4264c4c85b9bd7c
arthurviegasr <arthurviegasr@gmail.com>
2021-10-27 01:27:52 -0300

    snapCrackle complete

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,22 @@
-// Seu código vai aqui
+function snapCrackle (maxValue){
+    let snap = "";
+    for (let num = 1; num <= maxValue; num++){
+        if (num%2 !== 0 && num%5 === 0){
+            snap += "SnapCrackle";
+        }
+        else if (num%5 === 0){
+            snap += "Crackle";
+        }
+        else if (num%2 !== 0){
+            snap += "Snap";
+        }
+        else{
+            snap += num;
+        }
+        if (num !== maxValue ){
+            snap += ", "
+        }
+    }
+    return snap;
+}
+console.log(snapCrackle());
