d3f17008ad9a0f896f537e927ad4574c31a1ecd2
arthurviegasr <arthurviegasr@gmail.com>
2022-02-22 23:42:45 -0300

    implementacao bonus

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,21 +1,57 @@
-function snapCrackle (maxValue){
+function snapCrackle(maxValue) {
     let snap = "";
-    for (let num = 1; num <= maxValue; num++){
-        if (num%2 !== 0 && num%5 === 0){
+    for (let num = 1; num <= maxValue; num++) {
+        if (num % 2 !== 0 && num % 5 === 0) {
             snap += "SnapCrackle";
         }
-        else if (num%5 === 0){
+        else if (num % 5 === 0) {
             snap += "Crackle";
         }
-        else if (num%2 !== 0){
+        else if (num % 2 !== 0) {
             snap += "Snap";
         }
-        else{
+        else {
             snap += num;
         }
-        if (num !== maxValue ){
+        if (num !== maxValue) {
             snap += ", "
         }
+    }
+    return snap;
+}
+console.log(snapCrackle());
+
+function isPrime(num) {
+    for (let i = 2; i < num; i++){
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return num > 1;
+}
+
+function snapCracklePrime(maxValue) {
+    let snap = "";
+    for (let num = 1; num <= maxValue; num++) {
+        if (num % 2 !== 0 && num % 5 === 0) {
+            snap += "SnapCrackle";
+        }
+        else if (num % 5 === 0) {
+            snap += "Crackle";
+        }
+        else if (num % 2 !== 0) {
+            snap += "Snap";
+        }
+        else if (isPrime(num)){
+            snap += "Prime";
+        }
+        else {
+            snap += num;
+        }
+        if (num !== maxValue) {
+                snap += ", "
+        }
+
     }
     return snap;
 }
